9c9be04ed049ef4841ebca1be55e167b0d8e7df5
Marcelo <Marcelo@DESKTOP-7205U74>
2019-03-06 23:51:29 -0300

    First Commit

---
+++ src/main/java/com/bolsadeideas/springboot/app/controllers/IndexController.java
@@ -0,0 +1,20 @@
+package com.bolsadeideas.springboot.app.controllers;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+	@Value("${application.controllers.mensaje}")
+	private String mensaje;
+	
+	@GetMapping("/")
+	public String hola(Model model) {
+		model.addAttribute("mensaje",this.mensaje);
+		return  "hola";
+	}
+	
+}
